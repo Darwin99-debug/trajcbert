69e7b36e4e0fb4c34d91b102e9607993c712ecd7
cllacourar <lacourarie.clara@courrier.uqam.ca>
2023-06-08 16:46:45 -0400

    Addition of tokens to bert's tokenizer (to finish to test)

--- add_token.py
+++ add_token.py
@@ -5,7 +5,6 @@ from transformers import BertModel, BertTokenizer
 
 #we read the data
 with open('/home/daril_kw/data/02.06.23/train_clean.json', 'r') as openfile:
-
  
     # Reading from json file
     json_loaded = json.load(openfile)
@@ -54,10 +53,60 @@ print(" Old embedding vector at the end of the embedding matrix")
 print(model.embeddings.word_embeddings.weight[-1, :]) 
 
 # Randomly generated matrix 
+#this loop is to add a random vector to the embedding matrix for each new token
+
+#TODO :verify if it is the right way to do it
+for i in range(len(list_token)):
+    with torch.no_grad():
+        model.embeddings.word_embeddings.weight[-1, :] = torch.zeros([model.config.hidden_size]) 
+
+
+  
+print(" New embedding vector at the end of the embedding matrix")
+print(model.embeddings.word_embeddings.weight[-1, :])
+
+
+
+#we save the new tokenizer
+tokenizer.save_pretrained('/home/daril_kw/data/02.06.23/tokenizer_clean')   
+
+#we can add the token of the contextual information we want ie the following columns of the dataframe : 'Call_type', 'Taxi_id', 'Timestamp'
+#but we need them all of them as strings and not as integers so we need to convert them except for the column 'Call_type' which is already a string
+
+contextual_info_token = []
+for i in range(len(data_clean)):
+    contextual_info_token.append(data_clean['Call_type'][i])
+    contextual_info_token.append(str(data_clean['Taxi_id'][i]))
+    contextual_info_token.append(str(data_clean['Timestamp'][i]))   
+
+#we remove the duplicates
+contextual_info_token = list(set(contextual_info_token))
+    
+
+#we add the new tokens to the tokenizer
+print("Length of the tokenizer before adding the new tokens")
+print(len(tokenizer))  # 28996 
+
+tokenizer.add_tokens(contextual_info_token)
+
+print("Length of the tokenizer after adding the new tokens")
+print(len(tokenizer))  # 28997 
 
   
 
-model.embeddings.word_embeddings.weight[-1, :] = torch.zeros([model.config.hidden_size]) 
+model.resize_token_embeddings(len(tokenizer))  
+
+# The new vector is added at the end of the embedding matrix 
+
+  
+print(" Old embedding vector at the end of the embedding matrix")
+print(model.embeddings.word_embeddings.weight[-1, :]) 
+
+# Randomly generated matrix 
+
+for i in range(len(contextual_info_token)):
+    with torch.no_grad():
+        model.embeddings.word_embeddings.weight[-1, :] = torch.zeros([model.config.hidden_size]) 
 
   
 print(" New embedding vector at the end of the embedding matrix")
