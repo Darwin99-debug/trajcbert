363d75c9229bd064f97a24555f9ce7b90b23e7b5
cllacourar <lacourarie.clara@courrier.uqam.ca>
2023-06-13 11:22:24 -0400

    probleme de type

--- data_formating_input.py
+++ data_formating_input.py
@@ -21,8 +21,17 @@ data_format=data_clean.copy()
 data_format['Tokenization'].apply(lambda x: isinstance(x,list)).value_counts()
 print("Verification du format des données effectuée.")
 #on va mettre dans une nouvelle colonne INPUT la concaténation dans cet ordre, du dernier élément de ligne sur la colonne Tokenization, la colonne CALL_TYPE, la colonne TAXI_ID,  et enfin la liste des éléments la ligne pour la colonne Tokenization jusqu'à l'avant avant dernier élément
-#on sépare les éléments de contxete (derneir élément de la colonne Tokenization, et les autres colonnes) du débu de la tokenization par <SEP> 
-data_format['INPUT'] = data_format['Tokenization'].apply(lambda x: x[-1]) + ' ' + str(data_format['CALL_TYPE']) + ' ' + str(data_format['TAXI_ID']) + '<SEP>' + data_format['Tokenization'].apply(lambda x: x[:-2])
+#on sépare les éléments de contxete (derneir élément de la colonne Tokenization, et les autres colonnes) du débu de la tokenization par <SEP>
+
+data_format['CALL_TYPE']=data_format['CALL_TYPE'].apply(lambda x: ' '+x)
+data_format['TAXI_ID']=data_format['TAXI_ID'].apply(lambda x: ' '+x)
+
+liste=[[] for i in range(len(data_format))]
+
+for i in range(len(data_format)):
+    for j in range(len(data_format['Tokenization'][i]:
+        liste[i].append(data_format['Tokenization'][i][j])
+data_format['INPUT'] = data_format['Tokenization'].apply(lambda x: x[-1])  + data_format['CALL_TYPE'].apply(lambda x: str(x)) + data_format['TAXI_ID'].apply(lambda x: str(x)) + '<SEP>' + 
 # la target sera l'avant dernier élément de la colonne Tokenization
 data_format['TARGET']=data_format['Tokenization'][-2]
 
@@ -32,7 +41,7 @@ data_format.drop(['Tokenization','CALL_TYPE','TAXI_ID'],axis=1,inplace=True)
 #on sauvegarde le dataframe dans un fichier dictionnaire
 data_format.to_dict('records')
 #et enfin on le sauvegarde dans un fichier json
-with open('/home/daril_kw/data/dataset_to_tokenize_w_timestamp.json', 'w') as outfile:  
+with open('/home/daril_kw/data/formatted_dataset_to_tokenize.json', 'w') as outfile:  
     json.dump(data_format.to_dict('records'), outfile)
 
 
