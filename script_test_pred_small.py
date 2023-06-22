7e9ecb4021c73bc5b06aa8a70b6af6ad496fe057
cllacourar <lacourarie.clara@courrier.uqam.ca>
2023-06-22 16:09:46 -0400

    updating for training from the server

---
+++ script_test_pred_small.py
@@ -0,0 +1,88 @@
+#now we test the model on the test set
+# Create the DataLoader for our training set : test_inputs, test_labels, test_masks 
+batch_size = 32
+
+prediction_data = TensorDataset(test_inputs, test_masks, test_labels)
+prediction_sampler = SequentialSampler(prediction_data)
+prediction_dataloader = DataLoader(prediction_data, sampler=prediction_sampler, batch_size=batch_size)
+
+# Prediction on test set
+
+print('Predicting labels for {:,} test sentences...'.format(len(test_inputs)))
+
+# Put model in evaluation mode
+model.eval()
+
+# Tracking variables 
+predictions , true_labels = [], []
+print("We predict")
+# Predict 
+for batch in prediction_dataloader:
+  # Add batch to GPU
+    batch = tuple(t.to(device) for t in batch)
+   
+  # Unpack the inputs from our dataloader
+    b_input_ids, b_input_mask, b_labels = batch
+  
+  # Telling the model not to compute or store gradients, saving memory and 
+  # speeding up prediction
+    with torch.no_grad():
+      # Forward pass, calculate logit predictions
+        outputs = model(b_input_ids, token_type_ids=None, 
+                      attention_mask=b_input_mask)
+
+    logits = outputs[0]
+
+  # Move logits and labels to CPU
+    logits = logits.detach().cpu().numpy()
+    label_ids = b_labels.to('cpu').numpy()
+  
+  # Store predictions and true labels
+    predictions.append(logits)
+    true_labels.append(label_ids)
+
+print('    DONE.')
+
+
+
+
+#print('Positive samples: %d of %d (%.2f%%)' % (df.label.sum(), len(df.label), (df.label.sum() / len(df.label) * 100.0)))
+
+
+
+
+
+
+
+
+from sklearn.metrics import matthews_corrcoef
+
+matthews_set = []
+
+# Evaluate each test batch using Matthew's correlation coefficient
+print('Calculating Matthews Corr. Coef. for each batch...')
+
+# For each input batch...
+for i in range(len(true_labels)):
+
+  # The predictions for this batch are a 2-column ndarray (one column for "0"
+  # and one column for "1"). Pick the label with the highest value and turn this
+  # in to a list of 0s and 1s.
+  pred_labels_i = np.argmax(predictions[i], axis=1).flatten()
+
+  # Calculate and store the coef for this batch.
+  matthews = matthews_corrcoef(true_labels[i], pred_labels_i)
+  matthews_set.append(matthews)
+
+
+# Combine the predictions for each batch into a single list of 0s and 1s.
+flat_predictions = [item for sublist in predictions for item in sublist]
+flat_predictions = np.argmax(flat_predictions, axis=1).flatten()
+
+# Combine the correct labels for each batch into a single list.
+flat_true_labels = [item for sublist in true_labels for item in sublist]
+
+# Calculate the MCC
+mcc = matthews_corrcoef(flat_true_labels, flat_predictions)
+
+print('MCC: %.3f' % mcc)
