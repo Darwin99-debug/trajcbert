c7241aaf0c78f2fc82b2b4747bdc5f67c19ed939
Daril Kengne <darilkengne@gmail.com>
2023-06-26 09:07:02 -0400

    Addition of jobs to run on calclu Quebec

---
+++ computing_code/model.py
@@ -0,0 +1,32 @@
+import transformers
+import torch.nn as nn
+import numpy as np
+
+
+
+# sleep for 10 seconds
+import time
+time.sleep(10)
+# create a random array with 1000 elements
+x = np.random.rand(1000)
+# compute the mean of the array
+y = np.mean(x)
+# print the result
+print(y)
+
+# sleep for 10 seconds
+time.sleep(10)
+
+# create a file called 'result.txt' and write the result in it
+with open('result.txt', 'w') as fp:
+    fp.write(str(y))
+
+
+
+# import data.json file and convert it to a dictionary
+import json
+with open('data.json') as json_file:
+    data = json.load(json_file)
+
+# print the dictionary
+print(data)
---
+++ computing_code/run_model.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --account=def-nkambou
+#SBATCH --time=0-00:10:00 # time (DD-HH:MM)
+
+
+# creation of the virtual environment
+virtualenv -p python3.6 venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+
+#  run the model located in mode.py
+python model.py
+
+# this code is to lunch the model on calcul quebec
