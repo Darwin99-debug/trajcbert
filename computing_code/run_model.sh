09bdd1f9838b7e997563f68e2b19495692a2e485
Daril Kengne <darilkengne@gmail.com>
2023-06-28 06:14:36 -0400

    Stable version

--- computing_code/run_model.sh
+++ computing_code/run_model.sh
@@ -10,15 +10,15 @@
 VENV_DIR=venv
 
 # creation of the virtual environment
-python3 -m venv --system-site-packages "$VENV_DIR"
+#python3 -m venv --system-site-packages "$VENV_DIR"
 source "$VENV_DIR"/bin/activate
 
 # pip install --upgrade pip
 # upgrate pip with python 3.7
-python3 -m pip install --upgrade pip
+#python3 -m pip install --upgrade pip
 
 
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
 
 pip3 list
 
