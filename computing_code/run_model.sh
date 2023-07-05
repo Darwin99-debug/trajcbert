e8bc05adce6db32bb2a933d4ed12b43b644832a3
Daril Kengne <darilkengne@gmail.com>
2023-07-05 12:08:46 -0400

    Correction of the mail address for notifications on end or failled jobs

--- computing_code/run_model.sh
+++ computing_code/run_model.sh
@@ -2,13 +2,12 @@
 #SBATCH --account=def-nkambou
 #SBATCH --time=0-00:3:00 # time (DD-HH:MM)
 #SBATCH --job-name=first_small_trajcbert_on_cpu_s
-#SBATCH --output=outputs/%x-%j.out 
 #SBATCH --error=errors/%x-%j.err
 #SBATCH --cpus-per-task=39 # number of cores
 #SBATCH --mem-per-cpu=16G
 #SBATCH --nodes=3 
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user= kengne_wambo.daril_raoul@courrier.uqam.ca
+#SBATCH --mail-user= lacourarie.clara@courrier.uqam.ca
 
 
 
