fb0e6cad433d836c811754c2277a1da954b9b530
Daril Kengne <darilkengne@gmail.com>
2023-07-07 16:39:56 -0400

    Save

--- computing_code/run_model.sh
+++ computing_code/run_model.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 #SBATCH --account=def-nkambou
 #SBATCH --time=0-00:3:00 # time (DD-HH:MM)
-#SBATCH --job-name=first_small_trajcbert_on_cpu_s
+#SBATCH --job-name=first_small_trajcbert_on_cpu_s_and_venv
 #SBATCH --error=errors/%x-%j.err
 #SBATCH --cpus-per-task=39 # number of cores
 #SBATCH --mem-per-cpu=16G
 #SBATCH --nodes=3 
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user= lacourarie.clara@courrier.uqam.ca
+#SBATCH --mail-user= kengne_wambo.daril_raoul@courrier.uqam.ca
 
 
 
-VENV_DIR=venv
+
+VENV_DIR=/home/daril/scratch/MYENV
 
 source "$VENV_DIR"/bin/activate
 
