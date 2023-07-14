e426cbbf5ead6b97559b97df61edf606c5d0173a
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-07-14 16:47:05 -0400

    version generalized

--- version2_suite_train_prepa_gene.py
+++ version2_suite_train_prepa_gene.py
@@ -43,7 +43,7 @@ len_context_info = len(data_format['CONTEXT_INPUT'][0].split(' '))
 #we separate the dataframe into train and test 
 data_train, data_test = train_test_split(data_format, test_size=0.2, random_state=2023)
 
-"""
+
 
 def prepare_train(dataframe, sixty_percent=0.4, thirty_percent=0.3, ten_percent=0.10, last_prob=0.1, sep_prob=0.1 ):
     #sixty_percent is the proportion of the time we will take the target between the 60% last tokens and the 30% last tokens
@@ -148,7 +148,7 @@ def prepare_train(dataframe, sixty_percent=0.4, thirty_percent=0.3, ten_percent=
         #we take the tokenization_2 column
         tokenization_2 = dataframe_sixty.iloc[i]['Tokenization_2']
         #we take the token that is between the 60% and the 30% last tokens
-        tokenization_2 = tokenization_2[-int(len(tokenization_2)*0.6):-int(len(tokenization_2)*0.3)]
+        tokenization_2 = tokenization_2[int(len(tokenization_2)*0.4):int(len(tokenization_2)*0.7)]
         #we take a random index in the list
         if len(tokenization_2) != 0:
             index = random.randint(0,len(tokenization_2)-1)
@@ -185,7 +185,7 @@ def prepare_train(dataframe, sixty_percent=0.4, thirty_percent=0.3, ten_percent=
 
     for i in range(len(dataframe_thirty)):
         tokenization_2 = dataframe_thirty.iloc[i]['Tokenization_2']
-        tokenization_2 = tokenization_2[-int(len(tokenization_2)*0.3):-int(len(tokenization_2)*0.1)]
+        tokenization_2 = tokenization_2[int(len(tokenization_2)*0.7):int(len(tokenization_2)*0.9)]
         if len(tokenization_2) != 0:
             index = random.randint(0,len(tokenization_2)-1)
         else:
@@ -216,7 +216,7 @@ def prepare_train(dataframe, sixty_percent=0.4, thirty_percent=0.3, ten_percent=
 
     for i in range(len(dataframe_ten)):
         tokenization_2 = dataframe_ten.iloc[i]['Tokenization_2']
-        tokenization_2 = tokenization_2[-int(len(tokenization_2)*0.1):-1]
+        tokenization_2 = tokenization_2[int(len(tokenization_2)*0.9):-1]
         if len(tokenization_2) != 0:
             index = random.randint(0,len(tokenization_2)-1)
             list_target_ten_data[i] = tokenization_2[index]
@@ -281,143 +281,4 @@ def prepare_train(dataframe, sixty_percent=0.4, thirty_percent=0.3, ten_percent=
 
 
 df_full = prepare_train(data_train, 0.45, 0.3,0.15,0.05,0.05)
-"""
-
-def prepare_train_wo_duplicate(dataframe, nb_categories=5, decal_gauche=False, decal_droite=False, uniforme=True):
-    for i in range(nb_categories):
-        #we create a list per category with the name of the category
-        globals()['list_category'+str(i)] = []
-        #in the above line, we use globals() to create a list with a string as name
-    
-    #we create the threshold for each category knowing that they go from 0.3 to 1 (the last token is excluded)
-    #tow categories are reserved for the last token (the destination) and the [SEP] token so we don't take them into account
-    # for exqmple, if ze have 5 categories, the uniform threshold would be (1-0.3)/(5-2) = 0.23333333333333334
-    #that means that the first category will concern length of trajectory from 0.3 to 0.5333333333333333, the second from 0.5333333333333333 to 0.7666666666666666 and the third from 0.7666666666666666 to 1
-    #we create a list of threshold
-    list_threshold = [0.3+i*((1-0.3)/(nb_categories-2)) for i in range(nb_categories-1)]
-    
-
-    #we create a seed to be able to reproduce the results
-    random.seed(2023)
-    #wealculate the number of rows that will fall into each category
-    #we keep it in variables so that we can use it later
-    for i in range(nb_categories):
-        globals()['nb_rows_category'+str(i)] = int(len(dataframe)/nb_categories)
-    
-    #due to the conversion of int, we may have a number of rows that is not equal to the number of rows of the dataframe
-    # if the sum of the nb_rows_category is not equal to the number of rows of the dataframe
-    #we add the missing rows to one of the categories randomly
-    while sum([globals()['nb_rows_category'+str(i)] for i in range(nb_categories)]) != len(dataframe):
-        #we choose the category randomly
-        index = random.randint(0,nb_categories-1)
-        #we add one row to the category
-        globals()['nb_rows_category'+str(index)] += 1
-
-    #we create a list of index of the dataframe
-    list_index = [i for i in range(len(dataframe))]
-    #we shuffle the list of index
-    random.shuffle(list_index)
-    #we create a list of index for each category
-    for i in range(nb_categories):
-        globals()['list_index_category'+str(i)] = []
-    #we fill the list of index for each category
-    for i in range(nb_categories):
-        #we fill the list of index for each category
-        globals()['list_index_category'+str(i)] = list_index[:globals()['nb_rows_category'+str(i)]]
-        #we remove the index that we just put in the list
-        list_index = list_index[globals()['nb_rows_category'+str(i)]:]
-    #we create a list of dataframe for each category
-    for i in range(nb_categories):
-        globals()['dataframe_category'+str(i)] = pd.DataFrame()
-        #we use reindex so that the columns of the dataframes will be the same as the columns of the dataframe in parameter
-        globals()['dataframe_category'+str(i)] = globals()['dataframe_category'+str(i)].reindex(columns=dataframe.columns)
-        #we fill the list of dataframe for each category with the rows of the dataframe that correspond to the number of rows that we want for each category
-        # ie nb_rows_category
-        #fill the dataframes 
-        #the append does not exist for dataframes, we use the concat function 
-        globals()['dataframe_category'+str(i)] = pd.concat([globals()['dataframe_category'+str(i)],dataframe.iloc[globals()['list_index_category'+str(i)]]],ignore_index=True)
-
-
-    #we create a list of targets and deb_traj for each category
-    for i in range(nb_categories):
-        globals()['list_target_category'+str(i)] = [0 for i in range(len(globals()['dataframe_category'+str(i)]))]
-        globals()['list_deb_traj_category'+str(i)] = [[] for i in range(len(globals()['dataframe_category'+str(i)]))]
-
-
-    #we do the treatment for each category except the two last categories
-    for i in range(nb_categories-2):
-        df = globals()['dataframe_category'+str(i)]
-        for j in range(len(df)):
-            #we take the tokenization_2 column
-            tokenization_2 = df.iloc[j]['Tokenization_2']
-            #we use the threshold corresponding to the category
-            tokenization_2 = tokenization_2[int(list_threshold[i]*len(tokenization_2)):int(list_threshold[i+1]*len(tokenization_2))]
-            #we take randomly a token in the tokenization_2 if it is not empty ie if the list is not emptyof tokens is not too small to be cut in nb_categories
-            if len(tokenization_2) != 0:
-                index = random.randint(0,len(tokenization_2)-1)
-            else:
-                index = -1
-            #we take the token
-            token = tokenization_2[index]
-            #we put the token in the list of target
-            globals()['list_target_category'+str(i)][j] = token
-            #we put the token before the target in the list of deb_traj : we add the token befor the first threshold fisrt
-            for k in df.iloc[j]['Tokenization_2'][:int(list_threshold[i]*len(df.iloc[j]['Tokenization_2']))]:
-                globals()['list_deb_traj_category'+str(i)][j].append(k)
-            #we add the token from tokenization_2 before the target to the list of deb_traj
-            for k in tokenization_2[:index]:
-                globals()['list_deb_traj_category'+str(i)][j].append(k)
-
-
-            
-    #for the penultimate category, we take the last token of the tokenization_2
-    i = nb_categories-2
-    df = globals()['dataframe_category'+str(i)]
-    for j in range(len(df)):
-        #we take the tokenization_2 column
-        tokenization_2 = df.iloc[j]['Tokenization_2']
-        #we take the last token
-        token = tokenization_2[-1]
-        #we put the token in the list of target
-        globals()['list_target_category'+str(i)][j] = token
-        #we put the token before the target in the list of deb_traj : we add the token of the whole tokenization_2 before the target
-        globals()['list_deb_traj_category'+str(i)][j] = df.iloc[j]['Tokenization_2'][:-1]
-
-    #for the last category, we take the as target the [SEP] token
-    i = nb_categories-1
-    df = globals()['dataframe_category'+str(i)]
-    for j in range(len(df)):
-        globals()['list_target_category'+str(i)][j] = '[SEP]'
-        #we put the token before the target in the list of deb_traj : we add the token of the whole tokenization_2
-        globals()['list_deb_traj_category'+str(i)][j] = df.iloc[j]['Tokenization_2']
-                
-    #we add the lists of target in the column target of the dataframe and same for deb_traj
-    globals()['dataframe_category'+str(i)]['TARGET'] = globals()['list_target_category'+str(i)]
-    globals()['dataframe_category'+str(i)]['DEB_TRAJ'] = globals()['list_deb_traj_category'+str(i)]
-
-    #we verifiy that for each category exept the last one dataframe'Tokenization_2'][i][len(dataframe['DEB_TRAJ'][i])]!=dataframe['TARGET'][i]
-    # wuere i goes from 0 to len(dataframe)
-    #and the dataframe is the dataframe_category
-    for i in range(nb_categories-1):
-        df = globals()['dataframe_category'+str(i)]
-        for j in range(len(df)):
-            if df['Tokenization_2'][j][len(df['DEB_TRAJ'][j])]!=df['TARGET'][j]:
-                print(i,j)
-
-    #we get the full dataframe 
-    dataframe_full=pd.concat([globals()['dataframe_category'+str(i)] for i in range(nb_categories)],ignore_index=True)
-    #we put it in the right order
-    dataframe_full = dataframe_full.reindex(columns=dataframe.columns)
-
-    return dataframe_full
-
-#we call the function
-df_full = prepare_train_wo_duplicate(data_train)
-
-            
-
-
-
-    
-
 
