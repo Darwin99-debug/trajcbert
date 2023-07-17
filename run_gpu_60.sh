9eda5e7767ea270e210c6bc4c797837e844d0a94
Daril Kengne <darilkengne@gmail.com>
2023-07-17 10:53:23 -0400

    Chaning of the job name

--- run_gpu_60.sh
+++ run_gpu_60.sh
@@ -3,7 +3,7 @@
 #SBATCH --gres=gpu:1
 # SBATCH --mem-per-gpu=16G
 #SBATCH --time=03:00:00 
-#SBATCH --job-name=first_small_trajcbert_on_gpu_s_and_venv_scipy
+#SBATCH --job-name=first_small_trajcbert_on_gpu_s_60
 #SBATCH --output=outputs/%x-%j.out
 #SBATCH --error=errors/%x-%j.err
 #SBATCH --cpus-per-task=16 # number of cores
