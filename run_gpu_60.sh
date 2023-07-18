ef223aeac53d28b5c97bff7891acc76e4f65e8a0
Daril Kengne <darilkengne@gmail.com>
2023-07-18 15:08:15 -0400

    Passing to 40GB on narval

--- run_gpu_60.sh
+++ run_gpu_60.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --account=def-nkambou
 #SBATCH --gres=gpu:1
-#SBATCH --mem=32G
+#SBATCH --mem=40G
 #SBATCH --time=03:00:00 
-#SBATCH --job-name=first_small_trajcbert_on_gpu_s_60_batch_size_32
+#SBATCH --job-name=first_small_trajcbert_on_gpu_s_60_batch_size_16
 #SBATCH --output=outputs/%x-%j.out
 #SBATCH --error=errors/%x-%j.err
 #SBATCH --cpus-per-task=16 # number of cores
