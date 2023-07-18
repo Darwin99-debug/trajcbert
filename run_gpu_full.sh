4979e854280e76890f3b1f49bb3cf5ce79c972f0
Daril Kengne <darilkengne@gmail.com>
2023-07-18 15:38:45 -0400

    Correction of the full version

---
+++ run_gpu_full.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --account=def-nkambou
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --time=03:00:00 
+#SBATCH --job-name=first_small_trajcbert_on_gpu_s_60_batch_size_16
+#SBATCH --output=outputs/%x-%j.out
+#SBATCH --error=errors/%x-%j.err
+#SBATCH --cpus-per-task=16 # number of cores
+#SBATCH --nodes=1 
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kengne_wambo.daril_raoul@courrier.uqam.ca
+
+
+
+
+module load scipy-stack/2023a
+VENV_DIR=$SLURM_TMPDIR/MYENV
+virtualenv  $VENV_DIR
+source $VENV_DIR/bin/activate
+pip install -r requirements.txt --no-index
+pip list
+
+python first_test_small_train_wo_loading_model_full.py
+
+
