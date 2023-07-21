f0606fc0b889710296c3d0ed82997c89b75f7b7e
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-07-21 15:20:04 -0400

    test to solve

--- suite_train_preparation_gene.py
+++ suite_train_preparation_gene.py
@@ -120,7 +120,9 @@ def fill_target_deb_traj(df_dict, nb_categories, list_threshold, target_dict, li
             #we take the tokenization_2 column
             tokenization_2 = df.iloc[j]['Tokenization_2']
             if type(tokenization_2) != list:
-                print(tokenization_2, type(tokenization_2), i, j)
+                #on suppriime la liste du dataframe
+                df=df.drop(j, axis=0)
+                break
             #we use the threshold corresponding to the category
             tokenization_2 = tokenization_2[int(list_threshold[i]*len(tokenization_2)):int(list_threshold[i+1]*len(tokenization_2))]
             #we take randomly a token in the tokenization_2 if it is not empty ie if the list is not emptyof tokens is not too small to be cut in nb_categories
