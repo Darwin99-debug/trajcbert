16b6f5695d6355b40e18cc0f388af4e8d1dac233
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-07-24 15:41:58 -0400

    correction test2

--- suite_train_preparation_gene.py
+++ suite_train_preparation_gene.py
@@ -303,8 +303,10 @@ def manage_separation(dataframe, list_index_to_separate):
     # Sort both list_index and list_index_to_separate based on the TRIP_ID values
     list_index = [idx for idx in dataframe_separated.index if dataframe_separated.loc[idx, 'TRIP_ID'] in trip_ids_to_separate]
     list_index_to_separate = sorted(list_index_to_separate, key=lambda x: x[0])
+
     modified_rows = []
 
+    # Rest of the code remains the same
     for i in range(len(list_index_to_separate)):
         row = dataframe_separated.loc[list_index[i]].copy()
         dataframe_separated.drop(list_index[i], inplace=True)
@@ -326,14 +328,8 @@ def manage_separation(dataframe, list_index_to_separate):
             new_row['Tokenization_2'] = list_traj[j]
             modified_rows.append(new_row)
 
-    for i in range(len(list_index_to_separate)):
-        idx_to_remove = [idx for idx in dataframe_separated.index if dataframe_separated.loc[idx, 'TRIP_ID'] == list_index_to_separate[i][0]]
-        dataframe_separated.drop(idx_to_remove, inplace=True)
-
-    for row in modified_rows:
-        dataframe_separated = pd.concat([dataframe_separated, row.to_frame().T])
-
-    dataframe_separated.reset_index(drop=True, inplace=True)
+    # Concatenate all the modified rows in one operation
+    dataframe_separated = pd.concat([dataframe_separated] + modified_rows, ignore_index=True)
 
     return dataframe_separated
 
