9daff4255f55203dc4cee2b984a4dd12c81888bf
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-07-24 16:06:00 -0400

    it failed, we try to understand the pb

--- suite_train_preparation_gene.py
+++ suite_train_preparation_gene.py
@@ -421,6 +421,9 @@ def verif_concatenation(df_full, df_sep):
         #we verify that the sum of the length of the trajectories of df is equal to the length of the original trajectory
         if sum_len_traj != len_traj:
             print(df_full['TRIP_ID'][i], sum_len_traj, len_traj)
+            print(df['Tokenization_2'])
+            print(df_full['Tokenization_2'][i])
+            raise ValueError('The concatenation of the trajectories is not equal to the original trajectory') 
 
         
 
