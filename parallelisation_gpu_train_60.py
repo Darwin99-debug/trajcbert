9a142bf4769a2aa8eedac675004b6c5b1fff43b6
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-07-27 16:35:57 -0400

    meme en changeant de version, probleme pytroch donc test sans spawn et join() a la place

--- parallelisation_gpu_train_60.py
+++ parallelisation_gpu_train_60.py
@@ -306,7 +306,7 @@ if __name__ == "__main__":
     
 
     world_size = torch.cuda.device_count()
-
+    """
     mp.spawn(main, args=(world_size, save_every, epochs, batch_size), nprocs=world_size, join=True)
     """
     children = []
@@ -316,7 +316,7 @@ if __name__ == "__main__":
         subproc.start()
 
     for i in range(world_size):
-        children[i].join()"""
+        children[i].join()
         
 
      
