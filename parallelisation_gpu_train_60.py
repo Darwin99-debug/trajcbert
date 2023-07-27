9689b872e7ceecde08edcace7c47cfab47dcbe64
Daril Kengne <darilkengne@gmail.com>
2023-07-27 12:03:32 -0400

    Correction of the loader

--- parallelisation_gpu_train_60.py
+++ parallelisation_gpu_train_60.py
@@ -65,8 +65,8 @@ def prepare_dataloader(dataset: Dataset,batch_size: int):
     return DataLoader(dataset, 
                       batch_size=batch_size, 
                       shuffle=False, # Must be False when using DistributedSampler beacause it is already shuffled
-                      pin_memory=True # Automatically put the fetched data Tensors in pinned memory, and thus enables faster data transfer to CUDA-enabled GPUs.
-        sampler = DistributedSampler(dataset) # Select a subset of the dataset (only works if shuffle=False)
+                      pin_memory=True, # Automatically put the fetched data Tensors in pinned memory, and thus enables faster data transfer to CUDA-enabled GPUs.
+                      sampler = DistributedSampler(dataset) # Select a subset of the dataset (only works if shuffle=False)
       
                     )
 
