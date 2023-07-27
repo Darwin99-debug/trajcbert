fb7c59599a3bac1dcf9637cac5c1edfeca575fb7
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-07-27 13:47:47 -0400

    adding test to the ganaralized version + AR data formating and AR testing

--- testing_AR.py
+++ testing_AR.py
@@ -4,6 +4,10 @@ from sklearn.metrics import matthews_corrcoef
 from transformers import BertForSequenceClassification
 import torch
 import numpy as np
+import h3
+import pandas as pd
+from tqdm import tqdm
+from sklearn.metrics import f1_score, matthews_corrcoef
 
 device = torch.device("cpu")
 
@@ -11,88 +15,126 @@ model_dir = '/home/daril_kw/data/model_trained_cpu_version'
 data_dir = '/home/daril_kw/data/data_format_v_small.csv'
 
 
+    
+def flat_accuracy(preds, labels):
+    """this function computes the accuracy of the predictions"""
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return np.sum(pred_flat == labels_flat) / len(labels_flat)
 
-#load the prediction_dataloader
-prediction_dataloader = torch.load('/home/daril_kw/data/pred_dataloader_v_small.pt')
+def flat_f1(preds, labels):
+    """this function computes the f1 score of the predictions"""
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return f1_score(labels_flat,pred_flat,average='macro')
 
-# we load the model
-model = BertForSequenceClassification.from_pretrained(model_dir)
-print("we evaluate")
-model.eval()
  
-# Tracking variables 
-predictions , true_labels, list_inputs_test = [], [], []
-print("We predict")
-# Predict 
-for batch in prediction_dataloader:
-  # Add batch to GPU
-    batch = tuple(t.to(device) for t in batch)
-   
-  # Unpack the inputs from our dataloader
-    b_input_ids, b_input_mask, b_labels = batch
-  
-  # Telling the model not to compute or store gradients, saving memory and 
-  # speeding up prediction
+def flat_matthews(preds, labels):
+    """this function computes the matthews correlation coefficient of the predictions"""
+    pred_flat = np.argmax(preds, axis=1).flatten()
+    labels_flat = labels.flatten()
+    return matthews_corrcoef(labels_flat,pred_flat)
+
+def test_autoregressively(prediction_dataloader, targets_input,target_dict, model):
+    """this function will predict the labels of the test set autoregressively"""
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)
+    model.eval()
+
+    all_predictions = [[] for i in range(len(prediction_dataloader.dataset))]
+    all_predictions_detokenized = [[] for i in range(len(prediction_dataloader.dataset))]
+    all_true_labels = [[] for i in range(len(prediction_dataloader.dataset))]
+    
     with torch.no_grad():
-      # Forward pass, calculate logit predictions
-        outputs = model(b_input_ids, token_type_ids=None, 
-                      attention_mask=b_input_mask)
-
-    logits = outputs[0]
-
-  # Move logits and labels to CPU
-    logits = logits.detach().cpu().numpy()
-    label_ids = b_labels.to('cpu').numpy()
-  
-  # Store predictions and true labels
-    predictions.append(logits)
-    true_labels.append(label_ids)
-
-  #Store the inputs
-
-    list_inputs_test.append(b_input_ids.tolist())
-
-print('    DONE.')
-
-
-
-
-matthews_set = []
-
-# Evaluate each test batch using Matthew's correlation coefficient
-print('Calculating Matthews Corr. Coef. for each batch...')
-
-pred_label= []
-# For each input batch...
-for i in range(len(true_labels)):
-
-  # The predictions for this batch are a 2-column ndarray (one column for "0"
-  # and one column for "1"). Pick the label with the highest value and turn this
-  # in to a list of 0s and 1s.
-  pred_labels_i = np.argmax(predictions[i], axis=1).flatten()
-  pred_label.append(pred_labels_i)
-  # Calculate and store the coef for this batch.
-  matthews = matthews_corrcoef(true_labels[i], pred_labels_i)
-  matthews_set.append(matthews)
-
-
-# Combine the predictions for each batch into a single list of 0s and 1s.
-flat_predictions = [item for sublist in predictions for item in sublist]
-flat_predictions = np.argmax(flat_predictions, axis=1).flatten()
-
-# Combine the correct labels for each batch into a single list.
-flat_true_labels = [item for sublist in true_labels for item in sublist]
-
-
-# Combine the inputs for each batch into a single list.
-flat_list_inputs_test = [item for sublist in list_inputs_test for item in sublist]
-
-# Calculate the MCC
-mcc = matthews_corrcoef(flat_true_labels, flat_predictions)
-
-print('MCC: %.3f' % mcc)
+        for batch in tqdm(prediction_dataloader):
+            batch = batch[0].to(device)
+            #we get 50% of the length of the trajectory
+            nb_tokens = int(batch.shape[1]/2)
+            #we are going to predict the second half of the trajectory autoregressively
+            for i in range(nb_tokens,len(batch[0])):
+                batch_i = batch[:,:nb_tokens+i]
+                #we add the last token predicted to the input
+                if i != 0:
+                  batch = torch.cat((batch, torch.tensor([all_predictions[i-1]])), dim=1)
+                #we padd the input so that it has the same length as the input of the model ie 512
+                batch = torch.nn.functional.pad(batch, (0,512-nb_tokens), 'constant', 0)
+                #we create the attention mask associated to the input that is going to be 1s for the tokens that are not padded and 0s for the tokens that are padded
+                #for that we concatenate a tensor of 1s of size nb_tokens and a tensor of 0s of size 512-nb_tokens
+                attention_mask = torch.cat((torch.ones(nb_tokens), torch.zeros(512-nb_tokens))).to(device)
+                #we get the target token for this iteration 
+                target_token = batch[:,nb_tokens+i]
+                #we predict the target token
+                outputs = model(batch, token_type_ids=None, attention_mask=attention_mask, labels=target_token)
+                #we get the logits
+                logits = outputs[1].detach().cpu().numpy()
+                #we get the predicted token
+                predicted_token = np.argmax(logits)
+                #we add the predicted token to the list of predictions
+                all_predictions[i].append(predicted_token)
+                #we get the detokenized predicted token
+                predicted_token_detokenized = h3.h3_to_geo(target_dict[predicted_token])
+                #we add the detokenized predicted token to the list of detokenized predictions
+                all_predictions_detokenized[i].append(predicted_token_detokenized)
+                #we get the true token
+                true_token = target_token.detach().cpu().numpy()
+                #we add the true token to the list of true tokens
+                all_true_labels[i].append(true_token)
+    return all_predictions, all_predictions_detokenized, all_true_labels
 
 
 
+def calculate_MAD_score(all_predictions_detokenized, all_true_labels_coord):
+  """this function calculates the MAD score on the coordinates of the predictions"""
+  scores_coord = []
+  for i in range(len(all_predictions_detokenized)):
+    scores_coord.append(np.median([np.abs(all_predictions_detokenized[i][j][0]-all_true_labels_coord[i][j][0]) + np.abs(all_predictions_detokenized[i][j][1]-all_true_labels_coord[i][j][1]) for j in range(len(all_predictions_detokenized[i]))]))
+  MAD_score = np.mean(scores_coord)
+  return MAD_score
+
+   
+   
+def calculate_MSE_score(all_predictions_detokenized, all_true_labels_coord):
+  """this function calculates the MSE score on the coordinates of the predictions"""
+  scores_coord_MSE = []
+  for i in range(len(all_predictions_detokenized)):
+    scores_coord_MSE.append(np.mean([(all_predictions_detokenized[i][j][0]-all_true_labels_coord[i][j][0])**2 + (all_predictions_detokenized[i][j][1]-all_true_labels_coord[i][j][1])**2 for j in range(len(all_predictions_detokenized[i]))]))
+  MSE_score = np.mean(scores_coord_MSE)
+  return MSE_score
 
 #save flat_list_inputs_test
+
+
+def main():
+  #load targets_input and targets_dict
+  targets_input = torch.load('/home/daril_kw/data/AR/targets_input_v_small_AR.pt')
+  targets_dict = torch.load('/home/daril_kw/data/AR/targets_dict_v_small_AR.pt')
+
+  #load the prediction_dataloader
+  prediction_dataloader = torch.load('/home/daril_kw/data/pred_dataloader_v_small.pt')
+
+  # we load the model
+  model = BertForSequenceClassification.from_pretrained(model_dir)
+  print("we evaluate")
+  model.eval()
+
+  #we predict
+  all_predictions, all_predictions_detokenized, all_true_labels = test_autoregressively(prediction_dataloader, targets_input, targets_dict, model)
+
+  #we get the list of the coordinates associated to all_true_labels
+  all_true_labels_coord = []
+  for i in range(len(all_true_labels)):
+    all_true_labels_coord.append([h3.h3_to_geo(targets_dict[all_true_labels[i][j]]) for j in range(len(all_true_labels[i]))])
+  
+  #we can compare the coordinates of the predictions with the coordinates of the true labels
+  #we use the MAD metric : we get the median of the absolute difference between the coordinates of the predictions and the coordinates of the true labels
+  MAD_score = calculate_MAD_score(all_predictions_detokenized, all_true_labels_coord)
+
+  #we get the scores on the coordinates using the MSE criterion
+  MSE_score = calculate_MSE_score(all_predictions_detokenized, all_true_labels_coord)
+
+  #the difference between the MSE and the MAD is that the MSE is more sensitive to outliers
+
+     
+      
+
