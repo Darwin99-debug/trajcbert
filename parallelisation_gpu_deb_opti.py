f496ddc12fd24b05d8ab9f9f7da1384489a0a4c0
Daril Kengne <darilkengne@gmail.com>
2023-07-28 13:33:19 -0400

    modification for the full version

--- parallelisation_gpu_deb_opti.py
+++ parallelisation_gpu_deb_opti.py
@@ -19,7 +19,7 @@ def truncation_rows(df, nb_rows):
 
 def add_tokenization_column(df, config):
     """Add a column with the tokenization of the POLYLINE column"""
-    df['Tokenization_2'] = df['POLYLINE'].apply(lambda x: [h3.geo_to_h3(x[i][0], x[i][1], config) for i in range(len(x))])
+    df['Tokenization_2'] = df['POLYLINE'].apply(lambda x: [h3.geo_to_h3(x[i][1], x[i][0], config) for i in range(len(x))])
     return df
 
 def extract_time_info(df):
@@ -197,7 +197,7 @@ def main():
     data_format = pd.DataFrame(data=json_loaded)
 
     #we keep only nb_rows rows
-    data_format = truncation_rows(data_format, nb_rows)
+    # data_format = truncation_rows(data_format, nb_rows)
 
     #we add the tokenization column
     data_format = add_tokenization_column(data_format, h3_config_size)
@@ -230,9 +230,9 @@ def main():
     model.resize_token_embeddings(len(tokenizer))
 
     #save the model, the tokenizer and the data in different files
-    model.save_pretrained(f"/home/daril_kw/data/model_before_training_opti_{nb_rows}")
-    data_format.to_json(f"/home/daril_kw/data/data_with_time_info_ok_opti2_{nb_rows}.json")
-    tokenizer.save_pretrained(f"/home/daril_kw/data/tokenizer_final_opti_{nb_rows}")
+    model.save_pretrained(f"/home/daril_kw/data/model_before_training_opti_full")
+    data_format.to_json(f"/home/daril_kw/data/data_with_time_info_ok_opti2_full.json")
+    tokenizer.save_pretrained(f"/home/daril_kw/data/tokenizer_final_opti_full")
 
     #we get the DEB_TRAJ and TARGET columns well formatted but without the special tokens [CLS] and [SEP]
     data_format = get_deb_traj_and_target(data_format)
@@ -241,13 +241,13 @@ def main():
     input_ids, attention_masks, targets, full_inputs = formatting_to_train(data_format, tokenizer)
     
     #save the lists full_inputs, inputs_ids, attention_masks and the targets in different files
-    with open(f"/home/daril_kw/data/input_ids_{nb_rows}_opti.pkl", 'wb') as fp:
+    with open(f"/home/daril_kw/data/input_ids_full_opti.pkl", 'wb') as fp:
         pickle.dump(input_ids, fp)
-    with open(f"/home/daril_kw/data/attention_masks_{nb_rows}_opti.pkl", 'wb') as fp:
+    with open(f"/home/daril_kw/data/attention_masks_full_opti.pkl", 'wb') as fp:
         pickle.dump(attention_masks, fp)
-    with open(f"/home/daril_kw/data/targets_{nb_rows}_opti.pkl", 'wb') as fp:
+    with open(f"/home/daril_kw/data/targets_full_opti.pkl", 'wb') as fp:
         pickle.dump(targets, fp)
-    with open(f"/home/daril_kw/data/full_inputs_{nb_rows}_opti.pkl", 'wb') as fp:
+    with open(f"/home/daril_kw/data/full_inputs_full_opti.pkl", 'wb') as fp:
         pickle.dump(full_inputs, fp)
 
 
