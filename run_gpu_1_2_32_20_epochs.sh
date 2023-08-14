bb35a5a7b9226052397caa22a9734203c5ee55d6
Daril Kengne <darilkengne@gmail.com>
2023-08-14 16:10:12 -0400

    Code for savings test data on half of data

--- run_gpu_1_2_32_20_epochs.sh
+++ run_gpu_1_2_32_20_epochs.sh
@@ -7,7 +7,7 @@
 #SBATCH --error=errors/%x-%j.err
 #SBATCH --cpus-per-task=16 # number of cores for each task
 #SBATCH --nodes=3 
-#SBATCH --mem=498G
+#SBATCH --mem=0
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kengne_wambo.daril_raoul@courrier.uqam.ca
 
