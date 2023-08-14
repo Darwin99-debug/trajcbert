ae3df442faee60393c5fc81c2a3d333574f3983a
Daril Kengne <darilkengne@gmail.com>
2023-08-14 11:53:50 -0400

    Correction of the running script for 3 nodes, 32 CPUs and 12 GPUs

--- run_gpu_1_2_32_20_epochs.sh
+++ run_gpu_1_2_32_20_epochs.sh
@@ -5,8 +5,9 @@
 #SBATCH --job-name=trajcbert_on_gpu_1_2_batch_size_32_4_DAYS_20_epochs
 #SBATCH --output=outputs/%x-%j.out
 #SBATCH --error=errors/%x-%j.err
-#SBATCH --cpus-per-task=16 # number of cores
-#SBATCH --mem=249GB 
+#SBATCH --cpus-per-task=32 # number of cores for each task
+#SBATCH --nodes=3 
+#SBATCH --mem=0 # this means reserving all available memory on the node
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=kengne_wambo.daril_raoul@courrier.uqam.ca
 
