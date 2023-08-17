dd36cd4187825e74cfcdab3625cdad76fe205cf2
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-08-17 15:14:16 -0400

    addinf [0] to add a token for the AR bcs this is a tensor

--- testing_AR.py
+++ testing_AR.py
@@ -125,7 +125,7 @@ def test_autoregressively(prediction_dataloader, model, min_traj_rate, target_di
           #we add the detokenized predicted token to the list of detokenized predictions
           #all_predictions_detokenized[batch_idx].append(predicted_token_detokenized)
           #we add the predicted to the input by replacing the first pad token by the predicted token and take the next true token as the target token
-          traj_i_padded[index_token_to_predict] = predicted_token
+          traj_i_padded[0][first_token_to_predict] = predicted_token
 
          
   #return all_predictions, all_predictions_detokenized, all_true_labels, all_true_labels_coord
