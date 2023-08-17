d0fadfc640169e07ac3a0f5b518cc53ccf3ffe85
Clara Lacourarie <lacourarie.clara@courrier.uqam.ca>
2023-08-17 13:17:30 -0400

    cattemps to manage pb with dim in the model

--- testing_AR.py
+++ testing_AR.py
@@ -103,7 +103,9 @@ def test_autoregressively(prediction_dataloader, model, min_traj_rate, target_di
         
           #we get the attention mask associated to the input that is going to be 1s for the tokens that are not padded and 0s for the tokens that are padded
           att_mask = torch.cat((torch.ones(len(traj_i)), torch.zeros(512-len(traj_i)))).to(device)
-          #we predict the target token
+          #we get the batch
+          traj_i_padded = traj_i_padded.unsqueeze(0)
+          #we get the outputs of the model
           outputs = model(traj_i_padded, token_type_ids=None, attention_mask=att_mask, labels=list_true_tokens[index_token_to_predict-first_token_traj].unsqueeze(0)) # we need the -first_token_traj because the labels are the true tokens of the trajectory from the point first_token_traj to the end of the trajectory
           #we get the logits
           logits = outputs[1].detach().cpu().numpy()
